34c6386316f99d32a8f7d5b95303ce3ddecbcb04
AdindaClaraPutriMaharani <114728017+AdindaClaraPutriMaharani@users.noreply.github.com>
2024-03-07 22:40:28 +0700

    Add files via upload

---
+++ OOP3/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Circle Bulat = new Circle("Bulat", 5.0);
+        Bulat.getInfo();
+
+        Rectangle Persegi = new Rectangle("Persegi", 4.0, 8.0);
+        Persegi.getInfo();
+    }
+}
+    
+
+
