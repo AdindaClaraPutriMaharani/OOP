aa5dc5e1529e1c4943d8fce66e84dde8357cebb5
AdindaClaraPutriMaharani <114728017+AdindaClaraPutriMaharani@users.noreply.github.com>
2024-03-07 23:19:11 +0700

    Update Shape.java

--- OOP3/Shape.java
+++ OOP3/Shape.java
@@ -2,20 +2,26 @@ public class Shape {
     protected double area;
     protected double perimeter;
     protected String name;
+    protected double side1;
+    protected double side2;
 
-    public void setName(String name){
+    public Shape(String name) {
         this.name = name;
     }
-    
-    public double getArea(){
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getArea() {
         return area;
     }
 
-    public double getPerimeter(){
+    public double getPerimeter() {
         return perimeter;
     }
 
-    public void getInfo(){
+    public void getInfo() {
         System.out.println("Name: " + name);
         System.out.println("Area: " + area);
         System.out.println("Perimeter: " + perimeter);
