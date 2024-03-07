c201863557ebbd693b41af0d475ebe55646a3672
AdindaClaraPutriMaharani <114728017+AdindaClaraPutriMaharani@users.noreply.github.com>
2024-03-07 23:17:10 +0700

    Update App.java

--- OOP3/App.java
+++ OOP3/App.java
@@ -5,6 +5,18 @@ public class App {
 
         Rectangle Persegi = new Rectangle("Persegi", 4.0, 8.0);
         Persegi.getInfo();
+
+        RightTriangle SegitigaSikuSiku = new RightTriangle("SegitigaSikuSiku", 6.0, 7.0);
+        SegitigaSikuSiku.getInfo();
+
+        EquilateralTriangle segitigaSamaSisi = new EquilateralTriangle("SegitigaSamaSisi", 4.0);
+        segitigaSamaSisi.getInfo();
+
+        IsoscelesTriangle segitigaSamaKaki = new IsoscelesTriangle("SegitigaSamaKaki", 4.0, 6.0);
+        segitigaSamaKaki.getInfo();
+
+        AnyTriangle segitigaSembarang = new AnyTriangle("SegitigaSembarang", 4.0, 4.0, 4.0);
+        segitigaSembarang.getInfo();
     }
 }
     
