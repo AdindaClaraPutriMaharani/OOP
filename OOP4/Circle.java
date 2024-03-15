456e256478d3dfcc887511ef7620278303a280d4
AdindaClaraPutriMaharani <114728017+AdindaClaraPutriMaharani@users.noreply.github.com>
2024-03-15 12:00:05 +0700

    Update Circle.java

--- OOP4/Circle.java
+++ OOP4/Circle.java
@@ -1,8 +1,10 @@
 public class Circle extends Shape {
+
     protected Double radius;
 
     public Circle(Double radius, String name) {
         super.setName(name);
+        this.radius = radius;
         this.calculateArea(radius);
         this.calculatePerimeter(radius);
     }
@@ -13,5 +15,5 @@ public class Circle extends Shape {
 
     public void calculatePerimeter(Double radius) {
         super.perimeter = 2 * Math.PI * radius;
-    }    
+    }
 }
