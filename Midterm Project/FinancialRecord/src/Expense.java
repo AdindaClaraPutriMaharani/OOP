df5a0dc367677a2e500757745685e6c9ee52bb9a
AdindaClaraPutriMaharani <114728017+AdindaClaraPutriMaharani@users.noreply.github.com>
2024-06-28 09:17:34 +0700

    Update Expense.java

--- Midterm Project/FinancialRecord/src/Expense.java
+++ Midterm Project/FinancialRecord/src/Expense.java
@@ -1,10 +1,14 @@
 public class Expense extends FinancialRecord {
     public Expense(double amount, int day, int month, int year, String description) {
         super(amount, day, month, year, description);
+        totalExpenses += amount;
     }
 
     @Override
-    public void details(){
-        super.details();
+    public void details() {
+        System.out.println("Expense:");
+        System.out.println("Amount: $" + amount);
+        System.out.println("Date: " + day + "/" + month + "/" + year);
+        System.out.println("Description: " + description);
     }
 }
