1002ae0ce839e540ab42c53c37687506aecfdc3a
AdindaClaraPutriMaharani <114728017+AdindaClaraPutriMaharani@users.noreply.github.com>
2024-06-28 09:18:36 +0700

    Update Income.java

--- Midterm Project/FinancialRecord/src/Income.java
+++ Midterm Project/FinancialRecord/src/Income.java
@@ -1,10 +1,14 @@
 public class Income extends FinancialRecord {
     public Income(double amount, int day, int month, int year, String description) {
         super(amount, day, month, year, description);
+        totalIncome += amount;
     }
 
     @Override
-    public void details(){
-        super.details();
+    public void details() {
+        System.out.println("Income:");
+        System.out.println("Amount: $" + amount);
+        System.out.println("Date: " + day + "/" + month + "/" + year);
+        System.out.println("Description: " + description);
     }
 }
